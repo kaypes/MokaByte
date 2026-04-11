5a68aed785dfc53a2cbf3b58206faea583e8159b
Kayque Pereira <114672278+kaypes@users.noreply.github.com>
2026-04-11 15:24:49 -0300

    feat(memory): add RGB palette and extend memory map layout
    - Introduce `Rgb` struct for color representation
    - Add 16-color predefined palette
    - Extend memory map to include palette and input state
    - Adjust padding to maintain total RAM size consistency

--- src/core/machine.zig
+++ src/core/machine.zig
@@ -6,19 +6,41 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
-const std = @import("std");
-
-const width = 256;
-const height = 192;
 pub const vram_size = 24 * 1024;
 pub const ram_size = vram_size + 104 * 1024;
+
+pub const Rgb = extern struct {
+    r: u8,
+    g: u8,
+    b: u8,
+};
+
+pub const palette = [_]Rgb{
+    .{ .r = 33, .g = 43, .b = 94 },
+    .{ .r = 99, .g = 111, .b = 178 },
+    .{ .r = 173, .g = 196, .b = 255 },
+    .{ .r = 255, .g = 255, .b = 255 },
+    .{ .r = 255, .g = 204, .b = 215 },
+    .{ .r = 255, .g = 127, .b = 189 },
+    .{ .r = 135, .g = 36, .b = 80 },
+    .{ .r = 255, .g = 45, .b = 64 },
+    .{ .r = 239, .g = 96, .b = 74 },
+    .{ .r = 255, .g = 216, .b = 119 },
+    .{ .r = 0, .g = 204, .b = 139 },
+    .{ .r = 0, .g = 90, .b = 117 },
+    .{ .r = 81, .g = 58, .b = 232 },
+    .{ .r = 25, .g = 186, .b = 255 },
+    .{ .r = 119, .g = 49, .b = 165 },
+    .{ .r = 185, .g = 124, .b = 255 },
+};
 
 pub const Memory = extern union {
     raw: [ram_size]u8,
 
     map: extern struct {
         vram: [vram_size]u8,
-
-        remaining: [104 * 1024]u8,
+        palette: [16]Rgb,
+        input: u64,
+        padding: [ram_size - vram_size - (16 * 3)]u8,
     },
 };
