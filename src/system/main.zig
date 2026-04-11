4b74e16e42f88ee56895af7768936fefa665f375
Kayque Pereira <114672278+kaypes@users.noreply.github.com>
2026-04-11 16:55:35 -0300

    feat: implement VRAM rendering and main loop
    - Add `drawFrame` to translate VRAM bytes and palette entries into screen pixels
    - Initialize machine memory and start the render loop in `main.zig`
    - Update `Renderer` to handle dynamic texture updates from image data

--- src/system/main.zig
+++ src/system/main.zig
@@ -6,8 +6,23 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
-const MOKA_128 = @import("MOKA_128");
+const rl = @import("raylib");
 const video = @import("video.zig");
+const core = @import("MOKA_128");
 
 pub fn main() !void {
+    var machine: core.machine.Memory = undefined;
+    @memset(machine.raw[0..], 0);
+
+    machine.map.palette = core.machine.palette;
+
+    machine.map.vram[0] = 0x12;
+    machine.map.vram[1] = 0x34;
+
+    var display = try video.Renderer.init("Moka Console");
+    defer display.deinit();
+
+    while (!rl.windowShouldClose()) {
+        display.drawFrame(&machine);
+    }
 }
--- src/system/video.zig
+++ src/system/video.zig
@@ -13,7 +13,19 @@ pub const Renderer = struct {
     const width = 256;
     const height = 192;
 
-    const border_color = rl.Color{ 33, 43, 94, 255 };
+    const black_color = rl.Color{
+        .r = 4,
+        .g = 13,
+        .b = 20,
+        .a = 255,
+    };
+
+    const white_color = rl.Color{
+        .r = 255,
+        .g = 255,
+        .b = 255,
+        .a = 255,
+    };
 
     scale: f32,
     screen_width: i32,
@@ -35,8 +47,8 @@ pub const Renderer = struct {
         rl.toggleFullscreen();
         rl.setTargetFPS(60);
 
-        const img = rl.genImageColor(width, height, border_color);
-        const tex = rl.loadTextureFromImage(img);
+        const img = rl.genImageColor(width, height, black_color);
+        const tex = try rl.loadTextureFromImage(img);
 
         rl.setTextureFilter(tex, .point);
 
@@ -50,6 +62,75 @@ pub const Renderer = struct {
             .image = img,
             .texture = tex,
         };
+    }
+
+    pub fn drawFrame(self: *Self, machine: *core.machine.Memory) void {
+        for (machine.map.vram, 0..) |byte, i| {
+            const pixel_a_idx = (byte >> 4) & 0x0F;
+            const pixel_b_idx = byte & 0x0F;
+
+            const rgb_a = machine.map.palette[pixel_a_idx];
+            const rgb_b = machine.map.palette[pixel_b_idx];
+
+            const color_a = rl.Color{
+                .r = rgb_a.r,
+                .g = rgb_a.g,
+                .b = rgb_a.b,
+                .a = 255,
+            };
+
+            const color_b = rl.Color{
+                .r = rgb_b.r,
+                .g = rgb_b.g,
+                .b = rgb_b.b,
+                .a = 255,
+            };
+
+            const x1 = @as(i32, @intCast((i * 2) % width));
+            const y1 = @as(i32, @intCast((i * 2) / width));
+            const x2 = @as(i32, @intCast((i * 2 + 1) % width));
+            const y2 = @as(i32, @intCast((i * 2 + 1) / width));
+
+            rl.imageDrawPixel(&self.image, x1, y1, color_a);
+            rl.imageDrawPixel(&self.image, x2, y2, color_b);
+        }
+
+        rl.updateTexture(self.texture, self.image.data);
+        rl.beginDrawing();
+        rl.clearBackground(black_color);
+
+        const sw_f32 = @as(f32, @floatFromInt(self.screen_width));
+        const sh_f32 = @as(f32, @floatFromInt(self.screen_height));
+        const int_w_f32 = @as(f32, @floatFromInt(width));
+        const int_h_f32 = @as(f32, @floatFromInt(height));
+
+        const dest = rl.Rectangle{
+            .x = (sw_f32 - (int_w_f32 * self.scale)) * 0.5,
+            .y = (sh_f32 - (int_h_f32 * self.scale)) * 0.5,
+            .width = int_w_f32 * self.scale,
+            .height = int_h_f32 * self.scale,
+        };
+
+        const source = rl.Rectangle{
+            .x = 0.0,
+            .y = 0.0,
+            .width = @as(f32, @floatFromInt(self.texture.width)),
+            .height = @as(f32, @floatFromInt(self.texture.height)),
+        };
+
+        rl.drawTexturePro(
+            self.texture,
+            source,
+            dest,
+            rl.Vector2{
+                .x = 0,
+                .y = 0,
+            },
+            0.0,
+            white_color,
+        );
+
+        rl.endDrawing();
     }
 
     pub fn deinit(self: *Self) void {
