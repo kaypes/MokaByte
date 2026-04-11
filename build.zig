51f3b5cd0a7519af2fb427bd9948262b8c9ecd6e
Kayque Pereira <114672278+kaypes@users.noreply.github.com>
2026-04-11 16:02:13 -0300

    refactor(build): refine module configuration and test setup
    - Remove unnecessary raylib dependency from the core module
    - Pass optimization settings to the core module
    - Reorganize test step logic for better readability

--- build.zig
+++ build.zig
@@ -43,10 +43,7 @@ pub fn build(b: *std.Build) void {
     const mod = b.addModule("MOKA_128", .{
         .root_source_file = b.path("src/core/root.zig"),
         .target = target,
-        .imports = &.{.{
-            .name = "raylib",
-            .module = raylib,
-        }},
+        .optimize = optimize,
     });
 
     const exe = b.addExecutable(.{
@@ -90,21 +87,23 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
+    const test_step = b.step(
+        "test",
+        "Run tests",
+    );
+
     const mod_tests = b.addTest(.{
         .root_module = mod,
     });
 
     const run_mod_tests = b.addRunArtifact(mod_tests);
+    test_step.dependOn(&run_mod_tests.step);
 
     const exe_tests = b.addTest(.{
         .root_module = exe.root_module,
     });
 
     exe_tests.root_module.linkLibrary(raylib_artifact);
-
     const run_exe_tests = b.addRunArtifact(exe_tests);
-    const test_step = b.step("test", "Run tests");
-
-    test_step.dependOn(&run_mod_tests.step);
     test_step.dependOn(&run_exe_tests.step);
 }
